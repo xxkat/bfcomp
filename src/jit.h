7730aff02c8961ef21dab30f62976827078d38b1
xxkat <shqword@gmail.com>
2023-05-22 05:22:46 +1000

    Refactor code and add support for '.'
    The assembler needs a lot of work; however, it meets the bare-minimum requirements for this compiler, so I'll let this slide for now.

--- src/jit.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string_view>
-
-namespace bfcomp {
-
-void execute(std::string_view code);
-
-}
--- src/main.cc
+++ src/main.cc
@@ -1,15 +1,41 @@
+#include <Windows.h>
 #include <cstdint>
 #include <cstdlib>
+#include <fstream>
 
-#include <jit.h>
-#include <util.h>
+#include <core/jit.h>
+#include <util/print.h>
 
-int main(int argc, const char* argv[]) {
+namespace {
+
+int32_t jit_execute(std::string_view source) {
+  using BfEntry = void (*)();
+
+  bfcomp::X86Assembler cc{};
+  bfcomp::jit_compile(cc, source);
+
+  const auto code{cc.finalize()};
+  const auto alloc{VirtualAlloc(nullptr, code.size(), MEM_COMMIT, PAGE_EXECUTE_READWRITE)};
+
+  if (!alloc) {
+    bfcomp::print("Failed to allocate memory!");
+    return EXIT_FAILURE;
+  } else {
+    std::memcpy(alloc, code.data(), code.size());
+    reinterpret_cast<BfEntry>(alloc)();
+    VirtualFree(alloc, 0, MEM_RELEASE);
+  }
+
+  return EXIT_SUCCESS;
+}
+
+}  // namespace
+
+int32_t main(int argc, const char* argv[]) {
   if (argc < 2) {
     bfcomp::print("Usage: {} <Inline Source>", *argv);
     return EXIT_FAILURE;
   } else {
-    bfcomp::execute(argv[1]);
-    return EXIT_SUCCESS;
+    return jit_execute(argv[1]);
   }
 }
